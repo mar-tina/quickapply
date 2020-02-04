6221231aae89d2f07608f34054deafc0b80089b3
mar-tina <martinakhaemba@gmail.com>
2020-02-04 18:01:02 +0300

    setup content.js

--- extension/src/content.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(function(message, callback) {
-  if (message == "changeColor") {
-    chrome.tabs.executeScript({
-      code: 'document.body.style.backgroundColor="orange"'
-    });
-  }
-});
